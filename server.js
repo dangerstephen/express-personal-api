62bce0535a8c5e1068dbd8f3985fdabfb5797ce2
DANGERSTEPHEN <DANGERSTEVE1@GMAILCOM>
2016-11-10 15:50:38 -0800

    added information into my personal api

--- server.js
+++ server.js
@@ -21,6 +21,26 @@ app.use(function(req, res, next) {
 
 var db = require('./models');
 
+var profile = {
+  name: "Stephen",
+  birthState: "arizona",
+  alive: true,
+  githubLink:"https://github.com/dangerstephen",
+  githubProfileImage: "https://avatars2.githubusercontent.com/u/22550925?v=3&s=460",
+  personalSiteLink:  "https://dangerstephen.github.io/",
+  currentCity: "San Francisco",
+  siblings: [
+    {name: "Sherman Dangerfield", relationship: "Brother"},
+    {name: "Alan Dangerfield ", relationship: "Brother"},
+    {name: "Philip Dangerfield", relationship: "Brother"},
+    {name: "Jacob Dangerfield", relationship: "Brother"},
+    {name: "Sarah Dangerfield", relationship: "Sister"}],
+  animals: [
+    {name:"zipper", type:"dog"},
+    {name:"pixel", type:"dog"}
+    ]
+};
+
 /**********
  * ROUTES *
  **********/
@@ -46,15 +66,19 @@ app.get('/api', function api_index(req, res) {
   // TODO: Document all your api endpoints below
   res.json({
     woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
-    message: "Welcome to my personal api! Here's what you need to know!",
-    documentationUrl: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
-    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    message: "Welcome to my Stephen's api! Here's what you need to know!",
+    documentationUrl: "https://github.com/dangerstephen/express_self_api/README.md", // CHANGE ME
+    baseUrl: "https://vast-plateau-97777.herokuapp.com/", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
       {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
       {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
     ]
   })
+});
+
+app.get('/api/profile', function index (req, res) {
+  res.json({profile});
 });
 
 /**********
