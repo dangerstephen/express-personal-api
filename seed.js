8d4c5a881fdeb33739e08df1939690a6b73b6eb0
DANGERSTEPHEN <DANGERSTEVE1@GMAILCOM>
2016-11-10 15:09:37 -0800

    added stuff to seed to see if it would load

--- seed.js
+++ seed.js
@@ -3,41 +3,6 @@
 
 var db = require('./models');
 
-//
-// var authors_list = [
-//   {
-//     name: "Harper Lee",
-//     alive: false
-//   },
-//   {
-//     name: "F Scott Fitzgerald",
-//     alive: false
-//   },
-//   {
-//     name: "Victor Hugo",
-//     alive: false
-//   },
-//   {
-//     name: "Jules Verne",
-//     alive: false
-//   },
-//   {
-//     name: "Sheryl Sandberg",
-//     alive: true
-//   },
-//   {
-//     name: "Tim Ferriss",
-//     alive: true
-//   },
-//   {
-//     name: "John Steinbeck",
-//     alive: false
-//   },
-//   {
-//     name: "William Shakespeare",
-//     alive: false
-//   }
-// ];
 
 var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
 
