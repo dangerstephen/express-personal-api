7d5e6f5fc6d988f5ab23e60173aea5388cb71a08
DANGERSTEPHEN <DANGERSTEVE1@GMAILCOM>
2016-11-10 14:53:19 -0800

    added stuff to seed to see if it would load

--- seed.js
+++ seed.js
@@ -3,49 +3,49 @@
 
 var db = require('./models');
 
-
-var authors_list = [
-  {
-    name: "Harper Lee",
-    alive: false
-  },
-  {
-    name: "F Scott Fitzgerald",
-    alive: false
-  },
-  {
-    name: "Victor Hugo",
-    alive: false
-  },
-  {
-    name: "Jules Verne",
-    alive: false
-  },
-  {
-    name: "Sheryl Sandberg",
-    alive: true
-  },
-  {
-    name: "Tim Ferriss",
-    alive: true
-  },
-  {
-    name: "John Steinbeck",
-    alive: false
-  },
-  {
-    name: "William Shakespeare",
-    alive: false
-  }
-];
-
-// var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
-
-// db.Campsite.create(new_campsite, function(err, campsite){
-//   if (err){
-//     return console.log("Error:", err);
+//
+// var authors_list = [
+//   {
+//     name: "Harper Lee",
+//     alive: false
+//   },
+//   {
+//     name: "F Scott Fitzgerald",
+//     alive: false
+//   },
+//   {
+//     name: "Victor Hugo",
+//     alive: false
+//   },
+//   {
+//     name: "Jules Verne",
+//     alive: false
+//   },
+//   {
+//     name: "Sheryl Sandberg",
+//     alive: true
+//   },
+//   {
+//     name: "Tim Ferriss",
+//     alive: true
+//   },
+//   {
+//     name: "John Steinbeck",
+//     alive: false
+//   },
+//   {
+//     name: "William Shakespeare",
+//     alive: false
 //   }
+// ];
 
-//   console.log("Created new campsite", campsite._id)
-//   process.exit(); // we're all done! Exit the program.
-// })
+var new_campsite = {description: "Sharp rocks. Middle of nowhere."}
+
+db.Campsite.create(new_campsite, function(err, campsite){
+  if (err){
+    return console.log("Error:", err);
+  }
+
+  console.log("Created new campsite", campsite._id)
+  process.exit(); // we're all done! Exit the program.
+})
